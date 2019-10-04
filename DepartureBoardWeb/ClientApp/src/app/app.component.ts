4fc164bb9424fefded9a39e4bde5d054012c1b2d
Ben Fletcher <ben.fletcher@iress.com>
2019-10-04 13:20:53 +0100

    Init

---
+++ DepartureBoardWeb/ClientApp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css', './fonts/ledfont/stylesheet.css']
+})
+export class AppComponent {
+  title = 'app';
+}

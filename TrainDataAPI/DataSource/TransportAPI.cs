fbd0be88f71f4f4c35c1960d135f4e9677db75ca
Ben <ben.fletcher@iress.com>
2019-10-05 17:39:34 +0100

    Trys

--- TrainDataAPI/DataSource/TransportAPI.cs
+++ TrainDataAPI/DataSource/TransportAPI.cs
@@ -60,21 +60,25 @@ namespace TrainDataAPI
 
                 foreach (var Jdeparture in allDepartures["all"])
                 {
-                    int.TryParse(Jdeparture["platform"].ToString(), out int platform);
-                    string operatorName = Jdeparture["operator_name"].ToString();
-                    DateTime.TryParse(date + " " + Jdeparture["aimed_departure_time"].ToString(), out DateTime aimedDepatureTime);
-                    if(aimedDepatureTime == DateTime.MinValue)
-                        DateTime.TryParse(date + " " + Jdeparture["aimed_arrival_time"].ToString(), out aimedDepatureTime);
-                    DateTime.TryParse(date + " " + Jdeparture["expected_departure_time"].ToString(), out DateTime expectedDepatureTime);
-                    if(expectedDepatureTime == DateTime.MinValue)
-                        DateTime.TryParse(date + " " + Jdeparture["expected_arrival_time"].ToString(), out expectedDepatureTime);
-                    string destination = Jdeparture["destination_name"].ToString();
-                    Enum.TryParse(Jdeparture["status"].ToString().ToUpper(), out Departure.ServiceStatus status);
-                    string origin = Jdeparture["origin_name"].ToString();
-                    string serviceTimeTable = Jdeparture["service_timetable"]["id"].ToString();
+                    try
+                    {
+                        int.TryParse(Jdeparture["platform"].ToString(), out int platform);
+                        string operatorName = Jdeparture["operator_name"].ToString();
+                        DateTime.TryParse(date + " " + Jdeparture["aimed_departure_time"].ToString(), out DateTime aimedDepatureTime);
+                        if (aimedDepatureTime == DateTime.MinValue)
+                            DateTime.TryParse(date + " " + Jdeparture["aimed_arrival_time"].ToString(), out aimedDepatureTime);
+                        DateTime.TryParse(date + " " + Jdeparture["expected_departure_time"].ToString(), out DateTime expectedDepatureTime);
+                        if (expectedDepatureTime == DateTime.MinValue)
+                            DateTime.TryParse(date + " " + Jdeparture["expected_arrival_time"].ToString(), out expectedDepatureTime);
+                        string destination = Jdeparture["destination_name"].ToString();
+                        Enum.TryParse(Jdeparture["status"].ToString().ToUpper(), out Departure.ServiceStatus status);
+                        string origin = Jdeparture["origin_name"].ToString();
+                        string serviceTimeTable = Jdeparture["service_timetable"]["id"].ToString();
 
-                    Departure departure = new Departure(stationName, stationCode, platform, operatorName, aimedDepatureTime, expectedDepatureTime, destination, status, origin, lastUpdated, serviceTimeTable, GetType());
-                    departures.Add(departure);
+                        Departure departure = new Departure(stationName, stationCode, platform, operatorName, aimedDepatureTime, expectedDepatureTime, destination, status, origin, lastUpdated, serviceTimeTable, GetType());
+                        departures.Add(departure);
+                    }
+                    catch { }
                 }
             }
             catch
@@ -98,20 +102,24 @@ namespace TrainDataAPI
 
                 foreach (var JStop in results["stops"])
                 {
-                    string stationCode = JStop["station_code"].ToString();
-                    string stationName = JStop["station_name"].ToString();
-                    int.TryParse((JStop["platform"]??"").ToString(), out int platform);
-                    Enum.TryParse(JStop["stop_type"].ToString(), out StationStop.StopType stopType);
-                    DateTime.TryParse($"{JStop["aimed_departure_date"].ToString()} {JStop["aimed_departure_time"].ToString()}", out DateTime aimedDepartureDate);
-                    if(aimedDepartureDate == DateTime.MinValue)
-                        DateTime.TryParse($"{JStop["aimed_arrival_date"].ToString()} {JStop["aimed_arrival_time"].ToString()}", out aimedDepartureDate);
+                    try
+                    {
+                        string stationCode = JStop["station_code"].ToString();
+                        string stationName = JStop["station_name"].ToString();
+                        int.TryParse((JStop["platform"] ?? "").ToString(), out int platform);
+                        Enum.TryParse(JStop["stop_type"].ToString(), out StationStop.StopType stopType);
+                        DateTime.TryParse($"{JStop["aimed_departure_date"].ToString()} {JStop["aimed_departure_time"].ToString()}", out DateTime aimedDepartureDate);
+                        if (aimedDepartureDate == DateTime.MinValue)
+                            DateTime.TryParse($"{JStop["aimed_arrival_date"].ToString()} {JStop["aimed_arrival_time"].ToString()}", out aimedDepartureDate);
 
-                    DateTime.TryParse($"{JStop["expected_departure_date"].ToString()} {JStop["expected_departure_time"].ToString()}", out DateTime expectedDepartureDate);
-                    if(expectedDepartureDate == DateTime.MinValue)
-                        DateTime.TryParse($"{JStop["expected_arrival_date"].ToString()} {JStop["expected_arrival_time"].ToString()}", out expectedDepartureDate);
+                        DateTime.TryParse($"{JStop["expected_departure_date"].ToString()} {JStop["expected_departure_time"].ToString()}", out DateTime expectedDepartureDate);
+                        if (expectedDepartureDate == DateTime.MinValue)
+                            DateTime.TryParse($"{JStop["expected_arrival_date"].ToString()} {JStop["expected_arrival_time"].ToString()}", out expectedDepartureDate);
 
-                    StationStop stop = new StationStop(stationCode, stationName, stopType, platform, aimedDepartureDate, expectedDepartureDate);
-                    stops.Add(stop);
+                        StationStop stop = new StationStop(stationCode, stationName, stopType, platform, aimedDepartureDate, expectedDepartureDate);
+                        stops.Add(stop);
+                    }
+                    catch { }
                 }
             }
             catch

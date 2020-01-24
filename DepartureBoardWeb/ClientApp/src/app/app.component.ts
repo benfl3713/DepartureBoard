2f6c3676ef694b4c7a39df6104569d0970b64a93
Ben Fletcher <ben.fletcher@iress.com>
2020-01-24 12:21:45 +0000

    Added Google Analytics

--- DepartureBoardWeb/ClientApp/src/app/app.component.ts
+++ DepartureBoardWeb/ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        (<any>window).ga('set', 'page', event.urlAfterRedirects);
+        (<any>window).ga('send', 'pageview');
+      }
+    });
+  }
 }

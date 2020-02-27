551d612d6cdd6e90a20e62bbc79df513c7a1e41e
benfl3713 <buses3713@gmail.com>
2020-02-27 18:47:35 +0000

    Added mkdir

--- .buildkite/publish.sh
+++ .buildkite/publish.sh
@@ -1,3 +1,4 @@
+mkdir /${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/deploy
 dotnet publish -c Release -o /${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/deploy
 cd /${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/deploy
 zip -r ../output.zip *

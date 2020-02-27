3ee271993846ce13e24cc137d886de8cc4ee70fb
benfl3713 <buses3713@gmail.com>
2020-02-27 19:07:20 +0000

    Changed to docker-compose

--- .buildkite/publish.sh
+++ .buildkite/publish.sh
@@ -1,8 +1,1 @@
-mkdir ./${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/deploy
-npm link @angular/cli
-npm install
-dotnet publish -c Release -o ./${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/deploy --runtime win-x64 --self-contained true
-cd ./${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/deploy
-zip -r ../output.zip *
-cd ..
-rm -r ./deploy
+docker-compose build

425b73441b708bc0e920106d023f783c37a9b752
benfl3713 <buses3713@gmail.com>
2020-02-27 18:46:09 +0000

    Publish Step

---
+++ .buildkite/publish.sh
@@ -0,0 +1,5 @@
+dotnet publish -c Release -o /${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/deploy
+cd /${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/deploy
+zip -r ../output.zip *
+cd /${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/
+rm -r /${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/deploy

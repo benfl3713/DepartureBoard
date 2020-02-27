cc4fd5d01e8ac8e6ce5d8bf042540b5bc83f4591
benfl3713 <buses3713@gmail.com>
2020-02-27 18:57:56 +0000

    Added Npm Steps

--- .buildkite/publish.sh
+++ .buildkite/publish.sh
@@ -1,5 +1,7 @@
 mkdir ./${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/deploy
-dotnet publish -c Release -o ./${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/deploy
+npm link @angular/cli
+npm install
+dotnet publish -c Release -o ./${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/deploy --runtime win-x64 --self-contained true
 cd ./${BUILDKITE_BUILD_NUMBER}/DepartureBoardWeb/deploy
 zip -r ../output.zip *
 cd ..

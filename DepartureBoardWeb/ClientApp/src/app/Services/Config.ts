bf76bd8a2605196d6fce80ab4c73aa4e07d625af
benfl3713 <buses3713@gmail.com>
2020-04-15 22:00:34 +0100

    Added Config option for using google analytics

---
+++ DepartureBoardWeb/ClientApp/src/app/Services/Config.ts
@@ -0,0 +1,15 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+
+export class Config {
+  public static LoadUseAnalytics(http: HttpClient): () => Promise<any> {
+    return () => http.get<boolean>("/api/Config/UseAnalytics").toPromise().then(useAnalytics => {
+      if (useAnalytics === true) { Config.StartTracking();}
+    });
+  }
+
+  private static StartTracking() {
+    (<any>window).ga('create', 'UA-140494832-4', 'auto');
+    (<any>window).ga('send', 'pageview');
+  }
+}

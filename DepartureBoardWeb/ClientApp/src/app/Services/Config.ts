ea0ddc4b0e6c47f0a6891b5206422c77b2dc7be4
Ben Fletcher <ben.fletcher@iress.com>
2020-07-23 10:49:21 +0100

    Added coming soon widget

--- DepartureBoardWeb/ClientApp/src/app/Services/Config.ts
+++ DepartureBoardWeb/ClientApp/src/app/Services/Config.ts
@@ -1,23 +1,32 @@
-import { HttpClient } from '@angular/common/http';
-import { CookieService } from 'ngx-cookie-service';
+import { HttpClient } from "@angular/common/http";
+import { CookieService } from "ngx-cookie-service";
 
 export class Config {
-  public static LoadUseAnalytics(http: HttpClient, cookieService: CookieService): () => Promise<any> {
-    return () => http.get<boolean>("/api/Config/UseAnalytics").toPromise().then(useAnalytics => {
-      if (useAnalytics === true) {
-        var cookie = cookieService.get("CookieScriptConsent");
-        if (cookie == "") { return;}
-        var cookieEnabled = JSON.parse(cookie)["action"];
-        if (cookieEnabled == "accept") {
-          Config.StartTracking();
-        }
-      }
-    }).catch(error => console.log(error));
+  public static LoadUseAnalytics(
+    http: HttpClient,
+    cookieService: CookieService
+  ): () => Promise<any> {
+    return () =>
+      http
+        .get<boolean>("/api/Config/UseAnalytics")
+        .toPromise()
+        .then((useAnalytics) => {
+          if (useAnalytics === true) {
+            var cookie = cookieService.get("CookieScriptConsent");
+            if (cookie == "") {
+              return;
+            }
+            var cookieEnabled = JSON.parse(cookie)["action"];
+            if (cookieEnabled == "accept") {
+              Config.StartTracking();
+            }
+          }
+        })
+        .catch((error) => console.log(error));
   }
 
   public static StartTracking() {
-    (<any>window).ga('create', 'UA-140494832-4', 'auto');
-    (<any>window).ga('send', 'pageview');
-    //LogRocket.init('led-departure-board/led-departure-board');
+    (<any>window).ga("create", "UA-140494832-4", "auto");
+    (<any>window).ga("send", "pageview");
   }
 }
---
+++ DepartureBoardWeb/ClientApp/src/app/widgets/coming-soon/coming-soon.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-widget-coming-soon",
+  templateUrl: "./coming-soon.component.html",
+  styleUrls: ["./coming-soon.component.css"],
+})
+export class ComingSoonWidgetComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}

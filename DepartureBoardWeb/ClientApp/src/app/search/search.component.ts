58a1264688691947272c738c855ce22dfe6ceaa7
Ben Fletcher <benfl3713@gmail.com>
2020-11-16 19:40:39 +0000

    Added search options

--- DepartureBoardWeb/ClientApp/src/app/search/search.component.ts
+++ DepartureBoardWeb/ClientApp/src/app/search/search.component.ts
@@ -1,50 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { FormControl } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-	selector: 'app-components-search',
-	templateUrl: './search.component.html',
-	styleUrls: ['./search.component.css']
+  selector: "app-components-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"],
 })
 export class SearchComponent implements OnInit {
-	stations: any;
-	searchBox = new FormControl();
-	filteredOptions: any;
+  stations: any;
+  searchBox = new FormControl();
+  isSingleboard = new FormControl(false);
+  isArrivals = new FormControl(false);
+  filteredOptions: any;
 
-	constructor(private http: HttpClient, private router: Router) { document.title = "Departure Board"; }
+  constructor(private http: HttpClient, private router: Router) {
+    document.title = "Departure Board";
+  }
 
-	ngOnInit(): void {
-		this.http.get("/api/StationLookup").subscribe(s => { this.stations = s;});
-	}
+  ngOnInit(): void {
+    this.http.get("/api/StationLookup").subscribe((s) => {
+      this.stations = s;
+    });
+  }
 
-	filter() {
-		var query = this.searchBox.value;
-		if (query) {
-			this.http.get("/api/StationLookup?query=" + query).subscribe(s => { this.filteredOptions = s;});
-	  }
-		else {
-			this.filteredOptions = Array<string>();
+  filter() {
+    const query = this.searchBox.value;
+    if (query) {
+      this.http.get("/api/StationLookup?query=" + query).subscribe((s) => {
+        this.filteredOptions = s;
+      });
+    } else {
+      this.filteredOptions = Array<string>();
     }
-	}
+  }
 
-	Search() {
-		var stationCode = this.getKeyByValue(this.stations, this.searchBox.value);
-		if (!stationCode) {
-			alert("Invalid Station");
-			return;
-		}
-		this.router.navigate(([stationCode]));
-	}
+  Search() {
+    const stationCode = this.getKeyByValue(this.stations, this.searchBox.value);
+    if (!stationCode) {
+      alert("Invalid Station");
+      return;
+    }
+
+    let prefixUrl = "";
+
+    if (this.isSingleboard.value == true) {
+      prefixUrl = prefixUrl + "singleboard/";
+    }
+    if (this.isArrivals.value == true) {
+      prefixUrl = prefixUrl + "arrivals/";
+    }
+
+    this.router.navigate([prefixUrl, stationCode]);
+  }
 
   private getKeyByValue(object, value) {
-	  return Object.keys(object).find(key => object[key] === value);
-	}
+    return Object.keys(object).find((key) => object[key] === value);
+  }
 
-	asIsOrder(a, b) {
-		return 1;
-	}
+  asIsOrder(a, b) {
+    return 1;
+  }
 }

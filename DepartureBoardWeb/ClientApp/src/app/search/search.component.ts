12a449bf0dd49e5ea49d8126e32507f61590cc15
Ben Fletcher <benfl3713@gmail.com>
2021-01-26 20:10:37 +0000

    Search Box Improvements

--- DepartureBoardWeb/ClientApp/src/app/search/search.component.ts
+++ DepartureBoardWeb/ClientApp/src/app/search/search.component.ts
@@ -42,8 +42,12 @@ export class SearchComponent implements OnInit {
     }
   }
 
-  Search() {
-    const stationCode = this.searchBox.value.code;
+  async Search() {
+    let stationCode = this.searchBox.value.code;
+    if (!stationCode) {
+      stationCode = await this.attemptCalculate();
+    }
+
     if (!stationCode) {
       alert("Invalid Station");
       return;
@@ -62,6 +66,28 @@ export class SearchComponent implements OnInit {
     }
 
     this.router.navigate([prefixUrl, stationCode]);
+  }
+
+  async attemptCalculate(): Promise<string> {
+    const stations = await this.stationLookup
+      .Search(this.searchBox.value)
+      .toPromise();
+
+    const codeMatchedStations = stations.filter(
+      (s) => s.code.toUpperCase() === this.searchBox.value.toUpperCase()
+    );
+    if (codeMatchedStations.length > 0) {
+      return codeMatchedStations[0].code;
+    }
+
+    const nameMatchedStations = stations.filter(
+      (s) => s.name.toLowerCase() === this.searchBox.value.toLowerCase()
+    );
+    if (nameMatchedStations.length > 0) {
+      return nameMatchedStations[0].code;
+    }
+
+    return null;
   }
 
   stationDisplay(station: Station) {

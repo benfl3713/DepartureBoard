f1d55365c85e76809c8a8bd0e139bbf05c51ebf7
benfl3713 <buses3713@gmail.com>
2021-06-04 18:41:02 +0100

    Update angular material

--- DepartureBoardWeb/ClientApp/src/polyfills.ts
+++ DepartureBoardWeb/ClientApp/src/polyfills.ts
@@ -61,4 +61,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-import 'hammerjs/hammer';
+//import 'hammerjs/hammer';

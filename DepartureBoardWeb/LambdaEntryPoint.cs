c8f0bac5797ae6d09649bded426f917f550f362f
benfl3713 <buses3713@gmail.com>
2021-06-17 20:33:39 +0100

    Fixed Lambda Entrypoint

--- DepartureBoardWeb/LambdaEntryPoint.cs
+++ DepartureBoardWeb/LambdaEntryPoint.cs
@@ -1,15 +1,23 @@
 ﻿using Amazon.Lambda.AspNetCoreServer;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace DepartureBoardWeb
 {
-    public class LambdaEntryPoint: APIGatewayProxyFunction
-    {
-        protected override void Init(IWebHostBuilder builder)
-        {
-	        Startup.IsLambda = true;
-            builder.UseStartup<Startup>()
-                .UseLambdaServer();
-        }
-    }
+	public class LambdaEntryPoint : APIGatewayProxyFunction
+	{
+		protected override void Init(IWebHostBuilder builder)
+		{
+			Startup.IsLambda = true;
+
+			builder.UseSerilog()
+				.ConfigureAppConfiguration(configurationBuilder =>
+				{
+					configurationBuilder.AddEnvironmentVariables();
+				})
+				.UseStartup<Startup>()
+				.UseLambdaServer();
+		}
+	}
 }

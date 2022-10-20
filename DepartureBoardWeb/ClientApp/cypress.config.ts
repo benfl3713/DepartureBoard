b0dfbac7da835d377cdea49d88e5c1c3fca907f6
benfl3713 <buses3713@gmail.com>
2022-10-20 21:16:30 +0100

    Added cypress testing

---
+++ DepartureBoardWeb/ClientApp/cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:4200",
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    scrollBehavior: "center",
+  },
+});

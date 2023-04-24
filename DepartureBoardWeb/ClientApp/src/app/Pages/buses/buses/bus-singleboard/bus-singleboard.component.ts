9076cfc0b1755211ebadf6c9986a84f4aeb0b535
ben.fletcher <ben.fletcher@iress.com>
2023-04-24 17:38:16 +0100

    Improved Styling

--- DepartureBoardWeb/ClientApp/src/app/Pages/buses/buses/bus-singleboard/bus-singleboard.component.ts
+++ DepartureBoardWeb/ClientApp/src/app/Pages/buses/buses/bus-singleboard/bus-singleboard.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { BusDepartureService } from "src/app/Services/bus-departure.service";
-import { ActivatedRoute } from "@angular/router";
-import { ToggleConfig } from "src/app/ToggleConfig";
-import { tap, catchError } from "rxjs/operators";
-import { NotifierService } from "angular-notifier";
-import { of } from "rxjs";
+import {Component, OnInit} from "@angular/core";
+import {BusDepartureService} from "src/app/Services/bus-departure.service";
+import {ActivatedRoute} from "@angular/router";
+import {ToggleConfig} from "src/app/ToggleConfig";
+import {tap, catchError} from "rxjs/operators";
+import {NotifierService} from "angular-notifier";
+import {of} from "rxjs";
 
 @Component({
   selector: "app-bus-singleboard",
@@ -17,25 +17,28 @@ export class BusSingleboardComponent implements OnInit {
     private route: ActivatedRoute,
     private notifierService: NotifierService
   ) {
-    route.params.subscribe(() => {
-      route.queryParams.subscribe(() => {
-        this.SetupBoard();
+    route.params.subscribe((par) => {
+      route.queryParams.subscribe((qur) => {
+        this.SetupBoard(par, qur);
       });
     });
   }
 
   atco: string;
   dataSource: string;
+  count: number = 4;
   noBoardsDisplay: boolean = false;
   stopName: string = "";
   showStopName: boolean =
     localStorage.getItem("settings_buses_showStopName") == "true" || false;
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
-  SetupBoard() {
-    this.atco = this.route.snapshot.paramMap.get("atco");
-    this.dataSource = this.route.snapshot.queryParamMap.get("dataSource");
+  SetupBoard(params, queryParams) {
+    this.atco = params.atco;
+    this.dataSource = queryParams.datasource;
+    this.count = queryParams.count ?? this.count;
     setInterval(() => this.GetDepartures(), 30000);
     ToggleConfig.LoadingBar.next(true);
     this.GetDepartures();
@@ -43,7 +46,7 @@ export class BusSingleboardComponent implements OnInit {
 
   GetDepartures() {
     this.busDepartureService
-      .GetDepartures(this.atco, 4, this.dataSource)
+      .GetDepartures(this.atco, this.count, this.dataSource)
       .pipe(tap(() => ToggleConfig.LoadingBar.next(false)))
       .pipe(
         tap((data) => {
@@ -69,5 +72,33 @@ export class BusSingleboardComponent implements OnInit {
     let minutes = Math.floor(msDifference / 1000 / 60) * -1;
 
     return minutes <= 0 ? "due" : `${minutes}min`;
+  }
+
+  GetLineColour(departure: any) {
+    if (this.dataSource !== "TFLTUBE") return;
+
+    if (this.tubeColours[departure.line]) {
+      return {'color': this.tubeColours[departure.line]}
+    }
+
+    return {};
+  }
+
+  tubeColours = {
+    Bakerloo: '#B36305',
+    Central: '#E32017',
+    Circle: '#FFD300',
+    District: '#00782A',
+    Elizabeth: '#6950a1',
+    "Hammersmith & City": '#F3A9BB',
+    Jubilee: '#A0A5A9',
+    Metropolitan: '#9B0056',
+    Piccadilly: '#003688',
+    Victoria: '#0098D4',
+    "Waterloo & City": '#95CDBA',
+    DLR: '#00A4A7',
+    "London Overground": '#EE7C0E',
+    "London Trams": '#84B817',
+    "Emirates Cable Car": '#E21836'
   }
 }

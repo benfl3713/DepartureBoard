e2cbf44a38d0fb9979d01281b1fc38788f77b895
Ben Fletcher <44288013+benfl3713@users.noreply.github.com>
2023-11-18 13:41:20 +0000

    Advertise new tube departures (#291)
    Co-authored-by: benfl3713 <buses3713@gmail.com>

--- DepartureBoardWeb/ClientApp/src/app/widgets/news/news.items.ts
+++ DepartureBoardWeb/ClientApp/src/app/widgets/news/news.items.ts
@@ -1,5 +1,10 @@
 export const NewsItems: NewsItem[] = [
   {
+    title: "🚄 NEW: Tube Trains!!",
+    content:
+      "TFL Tube Departures are here in beta!!!. You can use them immediately by going to <a href='/london-tube/search'>Boards > London Tube</a> <span style=\"margin-left: 15px; font-size: 15px\" class=\"badge badge-pill badge-primary\">Beta</span>",
+  },
+  {
     title: 'New Framework Version',
     content:
       "Big Framework Version Update with increased performance. Please contact us if you see any issues"
@@ -10,12 +15,7 @@ export const NewsItems: NewsItem[] = [
       "A new <a href='https://docs.leddepartureboard.com' target='_blank'>website</a> has been built to provide documentation/user guides on leddepartureboard.com",
     isHighlighted: false,
     titleLink: 'https://docs.leddepartureboard.com'
-  },
-  {
-    title: "🚄 Coming Soon: Tube Trains",
-    content:
-      "TFL Tube Departures are coming soon!!!",
-  },
+  }
 ];
 
 interface NewsItem {
